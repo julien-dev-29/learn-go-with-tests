ec995f6728daf08a9b234776268fa76a4736fa2f
jurol <rollandjulien718@gmail.com>
2026-09-22 19:26:49 +0200

    feat: testing structures

---
+++ structs/shapes_test.go
@@ -0,0 +1,32 @@
+package structs
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "rectangle", shape: Rectangle{12, 6}, hasArea: 72},
+		{name: "circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "trianlge", shape: Triangle{12, 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.hasArea {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+		}
+	}
+}
